fcc77d9c801aa404f355f2447f11f54a6b006c97
Maz <mazicky@gmail.com>
2016-06-09 23:29:31 +0900

    Pass ASAP

--- src/utils/bowling.js
+++ src/utils/bowling.js
@@ -1,4 +1,15 @@
 
 export default function getBowlingScore(input) {
-  return parseInt(input)
+  let score = 0
+  for (let i=0; i<input.length; ++i) {
+    const ball = input.charAt(i)
+    const ballScore = parseInt(ball)
+    if (!isNaN(ballScore) && '' !== ballScore) {
+      score += ballScore
+    } else if (ball === '/') {
+      return 10
+    }
+  }
+
+  return score
 }
