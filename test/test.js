8297633ad4c1153a58da583eebabf3f32269e6bf
Maz <mazicky@gmail.com>
2016-06-09 23:17:28 +0900

    Change Test correctly

--- test/test.js
+++ test/test.js
@@ -4,8 +4,8 @@ import getBowlingScore from '../src/utils/bowling'
 chai.should()
 
 describe ('Test', () => {
-  it ('1frame game', () => {
-    getBowlingScore('1').should.equal(1)
-    getBowlingScore('2').should.equal(2)
+  it ('1 frame game', () => {
+    getBowlingScore('1-').should.equal(1)
+    getBowlingScore('1/').should.equal(10)
   })
 })
